c1cf6337453a2132d30a931a9787ffa5f1de0074
jackmou7 <jackmou7@gmail.com>
2024-07-26 10:27:45 +0800

    Add chip section and adjust scroll text effects

--- assets/x3proto.js
+++ assets/x3proto.js
@@ -12,3 +12,18 @@ $(document).on('scroll', function () {
     }
   }
 });
+
+const prev = document.getElementById('prev-btn');
+const next = document.getElementById('next-btn');
+const list = document.getElementById('item-list');
+
+const itemWidth = 150;
+const padding = 10;
+
+prev.addEventListener('click', () => {
+  list.scrollLeft -= itemWidth + padding;
+});
+
+next.addEventListener('click', () => {
+  list.scrollLeft += itemWidth + padding;
+});
